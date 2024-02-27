89a0b630a942674bca59eb484d947e16d75f73b8
keikenny <118848006+jokimi@users.noreply.github.com>
2024-02-27 22:34:22 +0300

    Add files via upload

---
+++ ТПвИ/lab2+/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"by.belstu.it.kozeka.basejava","l":"JavaTest"},{"p":"by.belstu.it.kozeka","l":"Main"},{"p":"by.belstu.it.kozeka","l":"TextFunction"},{"p":"by.belstu.it.kozeka.basejava","l":"WrapperString"}];updateSearchResults();
